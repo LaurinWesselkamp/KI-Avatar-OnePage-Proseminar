bfaa82933af4c3cc16bdb3a69106c6ac260d3bd3
LaurinWesselkamp <l.wesselkamp@icloud.com>
2022-06-19 19:12:45 +0200

    Spracherkennung workt TimerMItInt

---
+++ Assets/Timer/SyncedCountdown.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Normal.Realtime;
+
+public class SyncedCountdown : RealtimeComponent<IntModel>
+{
+    public int startingTime = 600;
+    public Text time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(model == null || model.value == 0.0)
+        {
+            time.text = "" + startingTime;
+        }
+        else
+        {
+            var rt = realtime.room.time;
+            var mt = model.value;
+            int ftime = startingTime - (int)(rt - mt);
+            time.text = ftime.ToString();
+        }
+    }
+
+    public void SetActivationTime()
+    {
+        model.value = (int) realtime.room.time;
+    }
+}
